b21f793ee55763cc1b02553ce92b27cba22e94c4
Алексей <52876090+AlexeyVodopyanov@users.noreply.github.com>
2020-10-17 15:02:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            ForegroundColor = ConsoleColor.Black;
+            int count = 1;
+            foreach (var color in colors)
+            {
+                BackgroundColor = color;
+                Write("{0,3} ", count++);
+                WriteLine(" " + color.ToString());
+            }
+
+        }
+    }
+}
